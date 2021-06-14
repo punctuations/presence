e18d7ca82102be4a6a0d74403231d9c408e82f98
Matt <matt@dont-ping.me>
2021-06-13 23:01:45 -0700

    feat(default themes):

---
+++ lib/types/ThemesTypes.ts
@@ -0,0 +1,11 @@
+export interface ThemesTypes {
+  [index: string]: {
+    bg: string;
+    text: string;
+    description: string;
+    stats: string;
+    statsText: string;
+    icon: string;
+    accent: string;
+  };
+}
