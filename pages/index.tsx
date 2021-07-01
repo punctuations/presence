4d0be650ed7a2bc343969347bdc4ea884ced97a2
matt <mattsnaccc@gmail.com>
2021-06-30 22:49:29 -0700

    refactor(showcase): only show one for each
    images in the showcase (for now) will only be talking of the project, if it can.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,25 +9,10 @@ import Docs from "@lib/ui/docs";
 import { Spacer } from "@geist-ui/react";
 
 export async function getServerSideProps() {
-  const twitterResponse = await fetch(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/twitter/user/atmattt?type=base64&rounded=true"
-      : "https://presence.vercel.app/api/twitter/user/atmattt?type=base64&rounded=true"
-  );
   const tweetResponse = await fetch(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/api/twitter/tweet/1394507872017403904?type=base64&theme=dark&rounded=true"
       : "https://presence.vercel.app/api/twitter/tweet/1394507872017403904?type=base64&theme=dark&rounded=true"
-  );
-  const twitterCardResponse = await fetch(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/twitter/card/atmattt?type=base64&rounded=true"
-      : "https://presence.vercel.app/api/twitter/card/atmattt?type=base64&rounded=true"
-  );
-  const githubCardResponse = await fetch(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/github/card/punctuations?type=base64&theme=dark&rounded=true"
-      : "https://presence.vercel.app/api/github/card/punctuations?type=base64&theme=dark&rounded=true"
   );
   const githubRepoResponse = await fetch(
     process.env.NODE_ENV === "development"
@@ -35,30 +20,18 @@ export async function getServerSideProps() {
       : "https://presence.vercel.app/api/github/repo/punctuations:presence?type=base64&rounded=true"
   );
 
-  const twitterBody = await twitterResponse.json(),
-    tweetBody = await tweetResponse.json(),
-    twitterCard = await twitterCardResponse.json(),
-    githubCard = await githubCardResponse.json(),
+  const tweetBody = await tweetResponse.json(),
     repoBody = await githubRepoResponse.json();
 
   return {
     props: {
-      twitter: twitterBody.data,
       tweet: tweetBody.data,
-      twitterCard: twitterCard.data,
-      githubCard: githubCard.data,
       repo: repoBody.data,
     },
   };
 }
 
-export default function Home(props: {
-  twitter?: string;
-  tweet?: string;
-  twitterCard?: string;
-  githubCard?: string;
-  repo?: string;
-}) {
+export default function Home(props: { tweet?: string; repo?: string }) {
   return (
     <>
       <Head>
@@ -91,13 +64,7 @@ export default function Home(props: {
 
       <Main>
         <Header />
-        <Images
-          twitter={props?.twitter as string}
-          tweet={props?.tweet as string}
-          twitterCard={props?.twitterCard as string}
-          githubCard={props?.githubCard as string}
-          repo={props?.repo as string}
-        />
+        <Images tweet={props?.tweet as string} repo={props?.repo as string} />
         <Spacer y={1} />
         <Docs text="🎉 Docs" url="/docs" />
       </Main>
