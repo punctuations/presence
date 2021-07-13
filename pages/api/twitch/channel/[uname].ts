8e01954c9991ec0cfce15c0859bc13985ef2de3a
matt <mattsnaccc@gmail.com>
2021-07-12 21:06:38 -0700

    fix(twitch): channel endpoint

---
+++ pages/api/twitch/channel/[uname].ts
@@ -0,0 +1,106 @@
+import { GithubUserImage } from "@github/user/image";
+
+import { base } from "@lib/components/base";
+import { convert } from "@lib/components/convert";
+
+import { NextApiRequest, NextApiResponse } from "next";
+import axios, { AxiosResponse } from "axios";
+import { TwitchUserImage } from "@lib/assets/twitch/user/image";
+import { Channel, TwitchChannelResponse } from "@lib/types/TwitchResponse";
+
+type Query = {
+  [p: string]: string | string[] | undefined;
+  bg?: string;
+  text?: string;
+  desc?: string;
+  stats?: string;
+  stats_text?: string;
+  theme?: string;
+  icon?: string;
+  rounded?: string;
+  type?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  return new Promise(async (resolve) => {
+    const query = req.query as Query,
+      uname = req.query.uname;
+
+    const request = await fetch(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_TWITCH_CLIENT_SECRET}&grant_type=client_credentials`,
+      {
+        method: "POST",
+      }
+    );
+
+    const auth = await request.json();
+
+    const channel = await fetch(
+      `https://api.twitch.tv/helix/search/channels?query=${uname}`,
+      {
+        headers: {
+          Authorization: `Bearer ${auth.access_token}`,
+          "Client-Id": `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
+        },
+      }
+    );
+    const twitch: TwitchChannelResponse = await channel.json();
+
+    let user: Channel;
+
+    for (let key in twitch.data) {
+      if (twitch.data[key].broadcaster_login === uname) {
+        user = twitch.data[key];
+      }
+    }
+
+    axios
+      .get(`https://api.twitch.tv/helix/users?login=${uname}`, {
+        headers: {
+          Authorization: `Bearer ${auth.access_token}`,
+          "Client-Id": `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
+        },
+      })
+      .then(async (r: AxiosResponse) => {
+        res.status(200);
+        res.send(
+          query.type?.toLowerCase() === "base64"
+            ? {
+                data: await base(await TwitchUserImage(user, r.data, query)),
+              }
+            : query.type?.toLowerCase() === "png"
+            ? await convert(
+                await TwitchUserImage(user, r.data, query),
+                955,
+                295
+              )
+            : await TwitchUserImage(user, r.data, query)
+        );
+        return resolve("Created Image!");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500);
+        res.send({ error: "Sorry, that user doesn't exist." });
+      });
+
+    query.type?.toLowerCase() !== "base64"
+      ? res.setHeader(
+          "Content-Type",
+          `image/${
+            query.type?.toLowerCase() === "png" ? "png" : "svg+xml"
+          }; charset=utf-8`
+        )
+      : null;
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader(
+      "content-security-policy",
+      "default-src 'none'; img-src * data:; style-src 'unsafe-inline'"
+    );
+  });
+}
