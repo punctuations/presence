ce5f06dd5fa29f5f1bf8408134593e3a4a9ca0cc
matt <mattsnaccc@gmail.com>
2021-07-15 01:37:07 -0700

    chore: update testers

--- lib/components/testers.ts
+++ lib/components/testers.ts
@@ -36,30 +36,3 @@ export const testers = [
 export function isTester(id: string): { id: string; name: string } | undefined {
   return testers.find((user) => user.id === id);
 }
-
-export type Testers = [
-  {
-    id: "705665813994012695";
-    name: "conrad";
-  },
-  {
-    id: "714863852441042965";
-    name: "fish";
-  },
-  {
-    id: "757834655516065862";
-    name: "haden";
-  },
-  {
-    id: "451261502138351627";
-    name: "kumal";
-  },
-  {
-    id: "291050399509774340";
-    name: "matt";
-  },
-  {
-    id: "204616460797083648";
-    name: "ven";
-  }
-];
