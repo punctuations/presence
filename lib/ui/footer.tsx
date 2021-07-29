532e17e8e4c6ce1ceb812b0d1a7aeaa6571c4721
matt <mattsnaccc@gmail.com>
2021-07-29 12:37:14 -0700

    feat(footer): add links to github, twitter, discord, email

--- lib/ui/footer.tsx
+++ lib/ui/footer.tsx
@@ -1,4 +1,5 @@
 import { Divider, Grid, Spacer } from "@geist-ui/react";
+import * as Icon from "@geist-ui/react-icons";
 
 export function Footer() {
   return (
@@ -7,9 +8,42 @@ export function Footer() {
       <Divider />
       <Grid.Container gap={2} justify="space-around" alignItems="center">
         <Grid xs={10}>
-          <a href="/">
-            <img style={{ width: "12rem" }} src="/presence.svg" alt="logo" />
-          </a>
+          <Grid.Container gap={2} justify="space-evenly" direction="column">
+            <Grid>
+              <a href="/">
+                <img
+                  style={{ width: "12rem" }}
+                  src="/presence.svg"
+                  alt="logo"
+                />
+              </a>
+            </Grid>
+
+            <Grid xs={9}>
+              <Grid.Container gap={0.75} justify="space-evenly" direction="row">
+                <Grid>
+                  <a href="https://github.com/punctuations/presence">
+                    <Icon.Github />
+                  </a>
+                </Grid>
+                <Grid>
+                  <a href="https://twitter.com/atmattt">
+                    <Icon.Twitter />
+                  </a>
+                </Grid>
+                <Grid>
+                  <a href="https://discord.gg/R3QtA68Cbf">
+                    <Icon.MessageCircle />
+                  </a>
+                </Grid>
+                <Grid>
+                  <a href="mailto:matt@dont-ping.me">
+                    <Icon.Mail />
+                  </a>
+                </Grid>
+              </Grid.Container>
+            </Grid>
+          </Grid.Container>
         </Grid>
         <Grid xs={6}>
           <a href="/privacy">Privacy Policy</a>
