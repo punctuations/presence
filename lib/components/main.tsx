bbfa0b780fc8374e99ed9507a5efb3b644eb6f8d
matt <matt@dont-ping.me>
2022-10-18 17:37:43 -0700

    revert: main

--- lib/components/main.tsx
+++ lib/components/main.tsx
@@ -4,7 +4,7 @@ import { Page, Grid } from "@geist-ui/react";
 
 export const Main = (props: { children: ReactNode }) => {
   return (
-    <Page dotBackdrop size="large">
+    <Page dotBackdrop>
       <Grid.Container gap={1} justify="center" alignItems="center">
         {props.children}
       </Grid.Container>
